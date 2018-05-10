0e2329b2f431c1a30394d3932a748d603053ecda
Faip Mardoni <faipmardoni@gmail.com>
2018-05-10 13:17:56 +0700

    login

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="test">
-    <p>hello world</p>
+  <div class="login-fb">
+    <p>a</p>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  name: "loginFB",
+  data: function() {
+    return "";
+  },
+  methods: {
+    
+  }
+};
 </script>
-
 <style>
 
 </style>
