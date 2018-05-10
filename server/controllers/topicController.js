a866777edc96a519bf3903b2369c15e9bcbd724a
Natascha Lamsu <tazcha15@gmail.com>
2018-05-10 19:11:25 +0700

    topic crud and endpoint

--- server/controllers/topicController.js
+++ server/controllers/topicController.js
@@ -11,14 +11,109 @@ module.exports = {
         if (!err) {
           res
             .status(201)
-            .send({
+            .json({
               message: 'added new topic',
               data: topic
             })
         } else {
           res
             .status(400)
-            .send(err)
+            .json(err)
+        }
+      })
+  },
+  readTopicByPhase: function(req, res) {
+    let token = req.headers.token;
+    jwt.verify(token, process.env.SECRET, function(err, decoded) {
+      topic
+      .findOne({
+        phase: decoded.phase
+      }, function(err, topic) {
+        if (!err) {
+          res
+            .status(200)
+            .json({topic})
+        } else {
+          res
+            .status(400)
+            .json({err})
+        }
+      })
+    })
+  },
+  readAllTopic: function(req, res) {
+    topic
+      .find()
+      .exec()
+      .then(topics => {
+        res
+          .status(200)
+          .json({
+            message: 'topic list',
+            topicData: topics
+          })
+      })
+      .catch(err => {
+        res
+          .status(400)
+          .json(err)
+      }) 
+  },
+  readTopicById: function(req, res) {
+    topic
+      .findById({
+        _id: req.params.id
+      }, function(err, result) {
+        if (!err) {
+          res
+            .status(200)
+            .json({
+              result
+            })
+        } else {
+          res
+            .status(400)
+            .json({
+              err
+            })
+        }
+      })
+  },
+  updateTopic: function(req, res) {
+    const { title, content, phase, image } = req.body
+
+    topic
+      .findByIdAndUpdate({
+        _id: req.params.id
+      }, req.body, function(err, updated) {
+        if (!err) {
+          res
+            .status(200)
+            .json({
+              updated
+            })
+        } else {
+          res
+            .status(400)
+            .json(err)
+        }
+      })
+  },
+  deleteTopic: function(req, res) {
+    topic
+      .findByIdAndRemove({
+        _id: req.params.id
+      }, function(err, deleted) {
+        if (!err) {
+          res
+            .status(200)
+            .json({
+              message: 'topic deleted'
+            })
+        } else {
+          res
+            .status(400)
+            .json(err)
         }
       })
   }
